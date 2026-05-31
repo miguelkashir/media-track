91ebad0eabcec8aaf9a0fa11792be50250a5b0bb
Miguel A. Alvarez <miguelkashir@gmail.com>
2026-05-31 18:43:24 +0200

    fix vercel analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { Outlet } from 'react-router';
 import { Nav } from './comps/Nav';
+import { Analytics } from '@vercel/analytics/next';
 
 export const App = () => {
   return (
     <div>
+      <Analytics />
       <Nav />
       <Outlet />
     </div>
--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,9 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { RouterProvider } from 'react-router';
 import { router } from './routes';
-import { Analytics } from '@vercel/analytics/next';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Analytics />
     <RouterProvider router={router} />
   </StrictMode>
 );
