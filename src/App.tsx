0bb65151a2f5dcde2d4e55b715343b24ff7ba476
Miguel A. Alvarez <miguelkashir@gmail.com>
2026-05-31 18:53:31 +0200

    remove vercel analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import { Outlet } from 'react-router';
 import { Nav } from './comps/Nav';
-import { Analytics } from '@vercel/analytics/next';
 
 export const App = () => {
   return (
     <div>
-      <Analytics />
       <Nav />
       <Outlet />
     </div>
